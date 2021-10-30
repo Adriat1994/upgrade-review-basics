b2732b55b5de82285e31913104aa51b6acd40fe6
Soy Adrián Torres <adriat1994@gmail.com>
2021-10-30 12:37:43 +0200

    Iteración 1

---
+++ Iteración 1_Mix for e includes.js
@@ -0,0 +1,19 @@
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+];
+
+let categoriasFiltradas = [];
+
+for (let categorias of movies) {
+
+    categorias.categories.forEach(element => {
+        
+        if (!categoriasFiltradas.includes(element)) {
+            categoriasFiltradas.push(element);           
+        }    
+    });     
+}
+console.log(categoriasFiltradas);
